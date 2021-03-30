e35de2bd424d9a8c95d2c2abad8e123624ee7c0d
sabine.hu <sabine.hu@epita.fr>
2021-03-31 00:32:38 +0200

    fix csg

--- src/init.cc
+++ src/init.cc
@@ -40,7 +40,7 @@ Camera init_camera() {
     /* Camera */
     //Vect look_from(6, 4, -8);
     //Vect look_from(0, 0, -6);
-    Vect look_from(0, 3, -5);
+    Vect look_from(2, 2, -5);
     //Vect look_from(0, 10, -0.0001); // Vu du dessus
     Vect look_at(0, 0, 0);
     Vect vup(0, 1, 0);
@@ -96,12 +96,43 @@ static void add_CSG_fig_1(Scene& scene) {
     scene.add_object(CSG_3);
 }
 
+static void add_CSG_fig_2(Scene& scene) {
+    auto lambertian_blue = std::make_shared<Lambertian>(blue, 0.6);
+    auto lambertian_blue2 = std::make_shared<Lambertian>(red, 0.6);
+    auto lambertian_blue3 = std::make_shared<Lambertian>(blue3, 0.6);
+
+    auto cube = std::make_shared<Cube>(Point3(-1, -1, -1), 2);
+    cube->set_texture(lambertian_blue);
+
+    auto sphere1 = std::make_shared<Sphere>(Point3(0, 0, 0), 1.25);
+    sphere1->set_texture(lambertian_blue3);
+
+    auto cylinder1 = std::make_shared<Cylinder>(Point3(-1.5, 0, 0), Point3(1.5, 0, 0), .25);
+    auto cylinder2 = std::make_shared<Cylinder>(Point3(0, 0, -1.5), Point3(0, 0, 1.5), .25);
+    auto cylinder3 = std::make_shared<Cylinder>(Point3(0, -1.5, 0), Point3(0, 1.5, 0), .25);
+    cylinder1->set_texture(lambertian_blue2);
+    cylinder2->set_texture(lambertian_blue2);
+    cylinder3->set_texture(lambertian_blue2);
+
+    auto CSG_1 = std::make_shared<CSG>(INTERSECTION, cube, sphere1);
+    auto CSG_2 = std::make_shared<CSG>(UNION, cylinder1, cylinder2);
+    auto CSG_3 = std::make_shared<CSG>(UNION, CSG_2, cylinder3);
+
+    auto CSG_4 = std::make_shared<CSG>(MINUS, CSG_1, CSG_3);
+
+    //scene.add_object(std::make_shared<CSG>(MINUS, sphere1, cylinder2));
+    scene.add_object(CSG_4);
+}
+
 void init_objects(Scene& scene) {
     /* Black and white plane */
     auto board_black_white = std::make_shared<CheckerBoard>(black, white);
     auto plane = std::make_shared<Plane>(Point3(0, 1, 0), -1);
     plane->set_texture(board_black_white);
     scene.add_object(plane);
+
+    //add_CSG_fig_2(scene);
+    add_CSG_fig_1(scene);
 
     /* Torus */
     //auto lambertian_random = std::make_shared<Lambertian>(r_random::random_color());
--- src/object/CSG.cc
+++ src/object/CSG.cc
@@ -21,8 +21,8 @@ bool CSG::find_intersection(const Ray& ray, double& t_min, double& t_max, Inters
     bool found2 = false;
     switch (op) {
         case UNION:
-            if (obj1->find_intersection(ray, t_min, t_max1, info)) found1 = true;
-            if (obj2->find_intersection(ray, t_min, t_max2, info)) found2 = true;
+            if (obj1->find_intersection(ray, t_min, t_max1, info1)) found1 = true;
+            if (obj2->find_intersection(ray, t_min, t_max2, info2)) found2 = true;
 
             /* No intersections */
             if (!found1 && !found2) return false;
@@ -32,16 +32,22 @@ bool CSG::find_intersection(const Ray& ray, double& t_min, double& t_max, Inters
                 if (t_max1 < t_max2) obj1_closest = true;
                 else obj1_closest = false;
                 t_max = std::min(t_max1, t_max2);
+                info.t_min = std::min(info1.t_min, info2.t_min);
+                info.t_max = std::max(info1.t_max, info2.t_max);
                 return true;
             }
 
             if (found1) {
                 t_max = t_max1;
+                info.t_min = info1.t_min;
+                info.t_max = info1.t_max;
                 obj1_closest = true;
             }
 
             if (found2) {
                 t_max = t_max2;
+                info.t_min = info2.t_min;
+                info.t_max = info2.t_max;
                 obj1_closest = false;
             }
             return true;
@@ -52,28 +58,38 @@ bool CSG::find_intersection(const Ray& ray, double& t_min, double& t_max, Inters
             if (!obj2->find_intersection(ray, t_min, t_max2, info2)) return false;
 
             // Get min t
-            if (info2.t_min < info1.t_min && info2.t_max > info1.t_min) {
+            if (info2.t_min < info1.t_min && info1.t_min < info2.t_max) {
                 t_max = info1.t_min;
                 obj1_closest = true;
+
+                info.t_min = info2.t_min;
+                info.t_max = info2.t_max;
                 return true;
-            } else if (info1.t_min < info2.t_min && info1.t_max > info2.t_min) {
+            } else if (info1.t_min < info2.t_min && info2.t_min < info1.t_max) {
                 t_max = info2.t_min;
                 obj1_closest = false;
+
+                info.t_min = info1.t_min;
+                info.t_max = info1.t_max;
                 return true;
             }
             return false;
 
         case MINUS:
             // Intersection for each obj
-            if (!obj1->find_intersection(ray, t_min, t_max1, info1)) return false;;
+            if (obj1->find_intersection(ray, t_min, t_max1, info1)) found1 = true;
             if (obj2->find_intersection(ray, t_min, t_max2, info2)) found2 = true;
 
-            if (!found2) {
-                t_max = t_max1;
-                obj1_closest = true;
-                return true;
+            if (!found1) return false;
+
+            /* Intersect both */
+            if (found2) {
+                return false;
             }
-            return false;
+
+            t_max = t_max1;
+            obj1_closest = true;
+            return true;
 
         default:
             if (obj1->find_intersection(ray, t_min, t_max1, info1)) found1 = true;
