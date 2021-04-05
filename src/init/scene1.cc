d2bcfe777769b350b815ea542b77ff1cf2a44798
sabine.hu <sabine.hu@epita.fr>
2021-04-05 18:35:50 +0200

    add scene1

--- src/init/scene1.cc
+++ src/init/scene1.cc
@@ -8,6 +8,16 @@
 #include "color.hh"
 
 #include "polygon.hh"
+#include "sphere.hh"
+#include "capsule.hh"
+#include "cube.hh"
+#include "rotate.hh"
+#include "ellipsoid.hh"
+#include "CSG.hh"
+
+#include "metal.hh"
+#include "lambertian.hh"
+
 #include "loader.hh"
 
 static Camera init_camera() {
@@ -42,14 +52,78 @@ static void add_water_plane(Scene& scene) {
     scene.add_object(p1);
 }
 
+static void add_sphere(Scene& scene) {
+    auto perlin = std::make_shared<PerlinNoise>(2, MARBLE, r_random::random_color() / 2, r_random::random_color());
+    auto obj1 = std::make_shared<Sphere>(Point3(3, -1, -4), 1);
+    obj1->set_texture(perlin);
+    obj1->add_bump_mapping(Noise(WOOD, 10));
+    scene.add_object(obj1);
+}
+
+static void add_sphere2(Scene& scene) {
+    auto t = std::make_shared<Metal>(r_random::random_color());
+    auto obj1 = std::make_shared<Sphere>(Point3(-5, -.5, 8), 1);
+    obj1->set_texture(t);
+    scene.add_object(obj1);
+}
+
+static void add_capsule(Scene& scene) {
+    auto metal_random = std::make_shared<Metal>(r_random::random_color(), 0.5);
+    auto capsule = std::make_shared<Capsule>(Point3(-6.5, -.75, 2), Point3(-4.5, -.75, 4), .75);
+    capsule->set_texture(metal_random);
+    capsule->add_bump_mapping(Noise(TURBULENCE, 10));
+    scene.add_object(capsule);
+}
+
+static void add_ellipsoid(Scene& scene) {
+    auto lambertian_random = std::make_shared<Lambertian>(light_orange, 0.6);
+    auto ellipsoid = std::make_shared<Ellipsoid>(Point3(-17, -1.25, 25), Point3(10, 1, 6));
+    ellipsoid->set_texture(lambertian_random);
+    scene.add_object(ellipsoid);
+}
+
+static void add_CSG_spheres(Scene& scene) {
+    auto perlin2 = std::make_shared<PerlinNoise>(2, TURBULENCE, r_random::random_color(), r_random::random_color());
+    perlin2->ka = 1;
+    perlin2->ks = 0.7;
+
+    auto sphere1 = std::make_shared<Sphere>(Point3(-3, -.8, -3.25), .75);
+    sphere1->set_texture(perlin2);
+    sphere1->set_specular(60);
+
+    auto sphere2 = std::make_shared<Sphere>(Point3(-4, -.8, -3), .75);
+    sphere2->set_texture(perlin2);
+    sphere2->set_specular(60);
+
+    auto res = std::make_shared<CSG>(UNION, sphere1, sphere2);
+    scene.add_object(res);
+}
+
+static void add_rotated_cube(Scene& scene) {
+    auto perlin4 = std::make_shared<PerlinNoise>(2, WOOD, Color(.314, .161, 0), Color(.392, .239, .004));
+    auto cube = std::make_shared<Cube>(Point3(-1, -2, -1), 2);
+    cube->set_texture(perlin4);
+
+    auto rotated1 = std::make_shared<RotatedObject>(cube, 45, AXIS_Y);
+    auto rotated2 = std::make_shared<RotatedObject>(rotated1, 20, AXIS_Z);
+    scene.add_object(rotated2);
+}
+
 Scene init_scene1() {
     Camera cam = init_camera();
 
     Scene scene(cam, Color(0.5, 0.5, 0.5));
     init_lights(scene);
 
-    add_boat(scene);
     add_water_plane(scene);
+
+    add_boat(scene);
+    add_rotated_cube(scene);
+    add_sphere(scene); // Bottom right
+    add_sphere2(scene);
+    add_capsule(scene);
+    add_ellipsoid(scene);
+    add_CSG_spheres(scene);
 
     return scene;
 }
--- src/main.cc
+++ src/main.cc
@@ -20,18 +20,18 @@ int main() {
 
     image::Image img = init_image(20, 1000);
 
-    Camera cam = init_camera();
-    Scene scene(cam, Color(0.5, 0.5, 0.5));
-    init_lights(scene);
+    //Camera cam = init_camera();
+    //Scene scene(cam, Color(0.5, 0.5, 0.5));
+    //init_lights(scene);
 
     /* TP1 */
     //init_objects(scene);
 
     /* TP2 */
-    init_objects2(scene);
-    init_blob(scene);
+    //init_objects2(scene);
+    //init_blob(scene);
 
-    //Scene scene = init_scene1();
+    Scene scene = init_scene1();
     //Scene scene = init_scene2();
     //Scene scene = init_scene3();
     render(img, scene, accuracy, samples, depth);
